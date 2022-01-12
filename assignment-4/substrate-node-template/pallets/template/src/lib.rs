226a099348b2ee58e3e657c741119ffd5252b513
lannn <nguyenngoclan.clc@gmail.com>
2022-01-12 13:53:47 +0700

    [Assignment 4] Add tweet struct

--- assignment-4/substrate-node-template/pallets/template/src/lib.rs
+++ assignment-4/substrate-node-template/pallets/template/src/lib.rs
@@ -6,7 +6,12 @@ pub use pallet::*;
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	use sp_std::vec::Vec;
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+	pub struct Tweet {
+		id: u8,
+		user_id: u8,
+	}
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -16,8 +21,8 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		ClaimCreated(T::AccountId, Vec<u8>),
-		ClaimRevoked(T::AccountId, Vec<u8>)
+		ClaimCreated(T::AccountId, Tweet),
+		ClaimRevoked(T::AccountId, Tweet)
 	}
 
 	#[pallet::error]
@@ -32,14 +37,14 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+	pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Tweet, (T::AccountId, T::BlockNumber), ValueQuery>;
 	
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(1_000)]
-		pub fn create_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+		pub fn create_claim(origin: OriginFor<T>, proof: Tweet) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
 			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
@@ -54,7 +59,7 @@ pub mod pallet {
 		}
 
 		#[pallet::weight(10_000)]
-		pub fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+		pub fn revoke_claim(origin: OriginFor<T>, proof: Tweet) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
 			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
